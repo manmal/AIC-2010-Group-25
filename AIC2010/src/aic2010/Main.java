fb1a483e80f468c359a4372cfc43d7b5b80f3e32
Manuel Maly <manuel.maly@gmail.com>
2010-10-11 18:27:55 +0200

    added more libraries

--- AIC2010/src/aic2010/Main.java
+++ AIC2010/src/aic2010/Main.java
@@ -5,6 +5,11 @@
 
 package aic2010;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.ws.Endpoint;
+import org.mortbay.jetty.Server;
+
 /**
  *
  * @author manuelmaly
@@ -16,6 +21,13 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Server server = new Server(8088);
+        try {
+            server.start();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
