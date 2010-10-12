11553a7bc3a374c4c9c59978bbe1935c125efeca
Manuel Maly <manuel.maly@gmail.com>
2010-10-12 11:04:18 +0200

    added code sample for jetty startup and endpoint publishing

---
+++ AIC2010/src/aic2010/codeSample/jettystartup/Customer.java
@@ -0,0 +1,20 @@
+
+package aic2010.codeSample.jettystartup;
+
+/**
+ * Debug use only!
+ * @author manuelmaly
+ */
+public class Customer {
+
+    protected String customerNumber;
+
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(String value) {
+        this.customerNumber = value;
+    }
+}
