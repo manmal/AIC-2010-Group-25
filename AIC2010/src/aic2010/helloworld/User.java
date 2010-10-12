3bb01c59d1e5faa74aaf71ed690a4dfc8c2e2503
Alex2k13 <alexander.bachinger@gmail.com>
2010-10-12 21:24:20 +0200

    Experimented with helloworld; Interfaces still make problems

--- AIC2010/src/aic2010/helloworld/User.java
+++
@@ -1,9 +0,0 @@
-package aic2010.helloworld;
-
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-
-@XmlJavaTypeAdapter(UserAdapter.class)
-public interface User {   
-
-    String getName();
-}
