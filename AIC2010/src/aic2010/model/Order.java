68485ecc991f60c5ffbc5a49c5059b63998219ad
Alex2k13 <alexander.bachinger@gmail.com>
2010-10-12 22:11:49 +0200

    Added XmlJavaTypeAdapter to Date field.

--- AIC2010/src/aic2010/model/Order.java
+++ AIC2010/src/aic2010/model/Order.java
@@ -1,11 +1,13 @@
 package aic2010.model;
 
+import aic2010.adapters.DateAdapter;
 import java.util.Date;
 import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 @XmlAccessorType(XmlAccessType.FIELD)   //serialize all fields by default
 @XmlRootElement(name="order")           //name root element
@@ -14,6 +16,9 @@ public class Order {
     //serialize this id as attribute
     @XmlAttribute(required=true)
     private String id;
+
+    //assign adapter class to represent Date as Long
+    @XmlJavaTypeAdapter(DateAdapter.class)
     private Date orderDate;
 
     private List<Item> items;
