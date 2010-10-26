cc7a62e52a411e60c3ad12782e4133c2fe3c7bd5
Rudolf Mildner <r.mildner@gmx.at>
2010-10-26 20:01:06 +0200

    adding WarehouseAnswer

---
+++ AIC2010/src/aic2010/model/WarehouseAnswer.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aic2010.model;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author rudolf
+ */
+@XmlRootElement(name="WarehouseAnswer")
+public class WarehouseAnswer {
+
+    @XmlAttribute(required = true)
+    private boolean isAvailable;
+
+    @XmlAttribute(required=true)
+    private int deliveryTime;
+
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(int deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
+    public boolean isIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final WarehouseAnswer other = (WarehouseAnswer) obj;
+        if (this.isAvailable != other.isAvailable) {
+            return false;
+        }
+        if (this.deliveryTime != other.deliveryTime) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 41 * hash + this.deliveryTime;
+        return hash;
+    }
+}
