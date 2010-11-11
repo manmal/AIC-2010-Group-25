bcb4148cd02a8b642fc1a43bfbb758768ecb35b2
Rudolf Mildner <r.mildner@gmx.at>
2010-11-11 20:12:34 +0100

    implementing WarehouseTest

--- AIC2010/test/aic2010/services/test/WarehouseTest.java
+++ AIC2010/test/aic2010/services/test/WarehouseTest.java
@@ -5,9 +5,13 @@
 
 package aic2010.services.test;
 
+import aic2010.TestDataManager;
+import aic2010.exception.UnknownProductException;
+import aic2010.model.Product;
+import aic2010.model.WarehouseAnswer;
 import aic2010.services.WarehouseService;
 import at.ac.tuwien.infosys.aic10.ass1.dto.warehouse.WarehouseServiceClient;
-import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,19 +31,45 @@ public class WarehouseTest {
         warehouse = client.getWarehousePT();
     }
 
-    @After
-    public void tearDown() {
-    }
+    @Test
+    public void checkKnownAvailableProduct()
+    throws UnknownProductException{
+        Product product1 = TestDataManager.getProduct(false, false);
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+        WarehouseAnswer answer = warehouse.check_availability(product1, 1);
+
+        Assert.assertEquals("Delivery time is not correct", 2, answer.getDeliveryTime());
+        Assert.assertEquals("Availability is not correct", true, answer.isIsAvailable());
+    }
 
     @Test
-    public void foo(){
-        
+    public void checkProductTooHighQuantity()
+    throws UnknownProductException{
+        Product product1 = TestDataManager.getProduct(false, false);
+
+        WarehouseAnswer answer = warehouse.check_availability(product1, 4);
+
+        Assert.assertEquals("Delivery time is not correct", 2, answer.getDeliveryTime());
+        Assert.assertEquals("Availability is not correct", false, answer.isIsAvailable());
     }
 
+    @Test
+    public void checkKnownUnavailableProduct()
+    throws UnknownProductException{
+        Product product2 = TestDataManager.getProduct2();
+
+        WarehouseAnswer answer = warehouse.check_availability(product2, 1);
+
+        Assert.assertEquals("Delivery time is not correct", 2, answer.getDeliveryTime());
+        Assert.assertEquals("Availability is not correct", true, answer.isIsAvailable());
+    }
+
+    @Test(expected=UnknownProductException.class)
+    public void checkUnknownProduct()
+    throws UnknownProductException{
+        Product unknownProduct = TestDataManager.getProduct(true, false);
+
+        //should throw an UnknownProductException
+        warehouse.check_availability(unknownProduct, 1);
+    }
 }
