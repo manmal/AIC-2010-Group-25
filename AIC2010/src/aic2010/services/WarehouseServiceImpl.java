88a1e72801a2eb18a0cfaa0880d4ad06441e7f4c
Rudolf Mildner <nanoquack@gmail.com>
2011-01-13 20:27:35 +0100

    Changing SupplierServiceImpl.java and WarehouseServiceImpl.java

--- AIC2010/src/aic2010/services/WarehouseServiceImpl.java
+++ AIC2010/src/aic2010/services/WarehouseServiceImpl.java
@@ -6,16 +6,17 @@
 package aic2010.services;
 
 import aic2010.TestDataManager;
+import aic2010.datastore.MiniDB;
 import aic2010.exception.UnknownProductException;
 import aic2010.model.Product;
 import aic2010.model.ProductEntry;
 import aic2010.model.WarehouseAnswer;
+import com.db4o.EmbeddedObjectContainer;
+import com.db4o.ObjectSet;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
-import javax.jws.WebResult;
 import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
 
 /**
  *
@@ -29,11 +30,10 @@ import javax.jws.soap.SOAPBinding;
 public class WarehouseServiceImpl extends SupplierServiceImpl implements WarehouseService{
 
     private Map<String, ProductEntry> products;
-    private Map<String, Product> actual_products;
 
     public WarehouseServiceImpl(){
             products = new HashMap<String, ProductEntry>();
-            actual_products = new HashMap<String, Product>();
+//            actual_products = new HashMap<String, Product>();
             addProducts();
     }
 
@@ -45,15 +45,12 @@ public class WarehouseServiceImpl extends SupplierServiceImpl implements Warehou
         entry1.setDeliveryTime(3);
         entry1.setAvailableAmount(3);
 
-        Product product2 = TestDataManager.getProduct2();
         ProductEntry entry2 = new ProductEntry();
-        entry2.setDeliveryTime(2);
-        entry2.setAvailableAmount(1);
+        entry1.setDeliveryTime(3);
+        entry1.setAvailableAmount(0);
 
-        products.put(product1.getId(), entry1);
-        products.put(product2.getId(), entry2);
-        actual_products.put(product1.getId(), product1);
-        actual_products.put(product2.getId(), product2);
+        products.put("1", entry1);
+        products.put("2", entry2);
     }
 
     @Override
@@ -86,13 +83,23 @@ public class WarehouseServiceImpl extends SupplierServiceImpl implements Warehou
     public BigDecimal order(Product product,
                         Integer amount)
     throws UnknownProductException{
-        if(actual_products.containsKey(product.getId())){
-            Product actual_product = actual_products.get(product.getId());
-            BigDecimal overallAmount = actual_product.getSingleUnitPrice().multiply(new BigDecimal(amount));
-            return overallAmount;
+        EmbeddedObjectContainer db = MiniDB.getDB();
+        Product actualProduct = null;
+
+        if(products.containsKey(product.getId())){
+           ObjectSet<Product> result = db.queryByExample(product);
+           if(result.hasNext()){
+               actualProduct = result.next();
+           }
+           else{
+               throw new UnknownProductException("Could not find product", product.getId());
+           }
         }
         else{
             throw new UnknownProductException("Could not find product", product.getId());
         }
+
+        BigDecimal overallAmount = actualProduct.getSingleUnitPrice().multiply(new BigDecimal(amount));
+        return overallAmount;
     }
 }
